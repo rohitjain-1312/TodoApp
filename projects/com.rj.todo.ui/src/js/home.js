c3648a7e50105a6321733324e24efae866775e9b
Rohit <rohitjain_1312@hotmail.com>
2016-10-14 23:41:24 -0700

    adding layout for todoApp

--- projects/com.rj.todo.ui/src/js/home.js
+++ projects/com.rj.todo.ui/src/js/home.js
@@ -2,10 +2,24 @@ var app = angular.module('rj.todo.app');
 
 app.controller('homeCtrl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
 
-  $scope.update = _update;
-  $scope.firstName = "";
+  $scope.removeTask = _removeTask;
+  $scope.addTask = _addTask;
+  _init();
 
-  function _update() {
-    $scope.firstName = $scope.firstName + " " + $scope.lastName;
+  function _init() {
+    $scope.tasks = [
+      {
+        title: "First Sample task"
+      }
+    ];
+  }
+
+  function _removeTask(taskIndex) {
+    $scope.tasks.splice(taskIndex, 1);
+  }
+
+  function _addTask() {
+    $scope.tasks.push({title: $scope.newTaskTitle});
+    $scope.newTaskTitle = null;
   }
 }]);
